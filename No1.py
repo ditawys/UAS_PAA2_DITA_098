66a6d148510e3e03975a1c283cbb9c82754bf648
Dita Widayanti Setiawan <115095585+ditawys@users.noreply.github.com>
2023-06-27 06:37:22 -0700

    Add files via upload

---
+++ No1.py
@@ -0,0 +1,86 @@
+import time
+
+def bubble_sort(arr):
+    n = len(arr)
+    start_time = time.time()
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    end_time = time.time()
+    return arr, end_time - start_time
+
+def insertion_sort(arr):
+    n = len(arr)
+    start_time = time.time()
+    for i in range(1, n):
+        key = arr[i]
+        j = i-1
+        while j >=0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    end_time = time.time()
+    return arr, end_time - start_time
+
+def print_iterations(arr, iterations):
+    print("Iterasi Pertama:")
+    for i in range(iterations):
+        print(arr[i], end=" ")
+    print("\n")
+    print("Iterasi Terakhir:")
+    for i in range(len(arr)-iterations, len(arr)):
+        print(arr[i], end=" ")
+    print("\n")
+
+def print_computation_time(time):
+    print("Waktu Komputasi Pengurutan:", time, "detik\n")
+
+def print_before_after(arr, sorted_arr):
+    print("Sebelum Pengurutan:", arr)
+    print("Setelah Pengurutan:", sorted_arr, "\n")
+
+def bubble_sort_analysis():
+    print("Analisis Bubble Sort:")
+    print("Worst case: O(n^2)")
+    print("Best case: O(n)")
+    print("Average case: O(n^2)\n")
+
+def insertion_sort_analysis():
+    print("Analisis Insertion Sort:")
+    print("Worst case: O(n^2)")
+    print("Best case: O(n)")
+    print("Average case: O(n^2)\n")
+
+def main():
+    arr = [12, 99, 62, 15, 20, 95, 39, 48, 3, 24, 8, 43, 74, 19, 97, 33, 49, 68, 55, 33, 90, 90, 7,
+           26, 85, 46, 39, 40, 9, 36, 60, 64, 89, 31, 25, 71, 21, 23, 63, 84, 32, 5, 3, 44, 21, 10, 21,
+           17, 50, 2, 36, 53, 79, 54, 19, 88, 1, 32, 31, 15, 6, 3, 1, 40, 22, 43, 18, 1, 77, 9, 59,
+           40, 7, 41, 81]
+
+    print("Pilih metode pengurutan:")
+    print("1. Bubble Sort")
+    print("2. Insertion Sort")
+    choice = int(input("Masukkan pilihan (1/2): "))
+
+    if choice == 1:
+        sorted_arr, time_taken = bubble_sort(arr.copy())
+        print_iterations(sorted_arr, 5)
+        print_computation_time(time_taken)
+        print_before_after(arr, sorted_arr)
+        bubble_sort_analysis()
+    elif choice == 2:
+        sorted_arr, time_taken = insertion_sort(arr.copy())
+        print_iterations(sorted_arr, 5)
+        print_computation_time(time_taken)
+        print_before_after(arr, sorted_arr)
+        insertion_sort_analysis()
+    else:
+        print("Pilihan tidak valid!")
+
+if __name__ == '__main__':
+    main()
---
+++ No2.py
@@ -0,0 +1,121 @@
+import time
+
+# Graf dalam bentuk dictionary
+graph = {
+    'a': {'b': 12, 'c': 10, 'g': 12},
+    'b': {'a': 12, 'd': 12, 'c': 8},
+    'c': {'a': 10, 'b': 8, 'd': 11, 'e': 3, 'g': 9},
+    'd': {'b': 12, 'c': 11, 'e': 11, 'f': 10},
+    'e': {'c': 3, 'd': 11, 'f': 6, 'g': 7},
+    'f': {'d': 10, 'e': 6, 'g': 9},
+    'g': {'a': 12, 'c': 9, 'e': 7, 'f': 9}
+}
+
+
+def tsp(graph, start, visited, curr_path, min_path, curr_cost, num_nodes):
+    if len(visited) == num_nodes and start in graph[curr_path[-1]]:
+        curr_path.append(start)
+        curr_cost += graph[curr_path[-2]][curr_path[-1]]
+        if curr_cost < min_path['cost']:
+            min_path['path'] = curr_path.copy()
+            min_path['cost'] = curr_cost
+        curr_path.pop()
+        curr_cost -= graph[curr_path[-1]][start]
+        return
+
+    for node in graph[curr_path[-1]]:
+        if node not in visited:
+            curr_path.append(node)
+            visited.add(node)
+            curr_cost += graph[curr_path[-2]][node]
+
+            tsp(graph, start, visited, curr_path, min_path, curr_cost, num_nodes)
+
+            curr_path.pop()
+            visited.remove(node)
+            curr_cost -= graph[curr_path[-1]][node]
+
+
+def tsp_main(graph):
+    start = input("Masukkan titik awal: ").lower()
+
+    if start not in graph:
+        print("Titik awal tidak valid.")
+        return
+
+    num_nodes = len(graph)
+    visited = set([start])
+    curr_path = [start]
+    min_path = {'path': [], 'cost': float('inf')}
+    curr_cost = 0
+
+    start_time = time.time()
+    tsp(graph, start, visited, curr_path, min_path, curr_cost, num_nodes)
+    end_time = time.time()
+
+    print("\nHasil TSP (Traveling Salesman Problem):")
+    print("Shortest path:", min_path['path'])
+    print("Total cost:", min_path['cost'])
+    print("Waktu komputasi:", end_time - start_time, "detik")
+
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    visited = set()
+
+    while len(visited) < len(graph):
+        min_distance = float('inf')
+        min_node = None
+
+        for node in graph:
+            if distances[node] < min_distance and node not in visited:
+                min_distance = distances[node]
+                min_node = node
+
+        visited.add(min_node)
+
+        for neighbor, weight in graph[min_node].items():
+            distance = distances[min_node] + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+
+    return distances
+
+
+def dijkstra_main(graph):
+    start = input("Masukkan titik awal: ").lower()
+
+    if start not in graph:
+        print("Titik awal tidak valid.")
+        return
+
+    start_time = time.time()
+    shortest_distances = dijkstra(graph, start)
+    end_time = time.time()
+
+    print("\nHasil Dijkstra:")
+    print("Titik awal:", start)
+    for node, distance in shortest_distances.items():
+        print("Titik", node, "- Jarak terpendek:", distance)
+
+    print("Waktu komputasi:", end_time - start_time, "detik")
+
+
+def main():
+    print("Pilih algoritma:")
+    print("1. TSP (Traveling Salesman Problem)")
+    print("2. Dijkstra")
+
+    choice = input("Masukkan pilihan (1/2): ")
+
+    if choice == '1':
+        tsp_main(graph)
+    elif choice == '2':
+        dijkstra_main(graph)
+    else:
+        print("Pilihan tidak valid. Silakan coba lagi.")
+
+
+if __name__ == '__main__':
+    main()
